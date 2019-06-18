317683d95f4831cba939b7e683829995c57e43a7
MatiasAlmeida <mati_sharck@hotmail.com>
2019-06-18 11:53:44 -0300

    Clase 24

---
+++ Clase24/FrmTest/FormTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmTest
+{
+    public partial class FormTest : Form
+    {
+        public FormTest()
+        {
+            Manejadora man = new Manejadora();
+            InitializeComponent();
+            //this.btnBoton.Click += new System.EventHandler(Manejadora.Manejador);
+            //this.lblEtiqueta.Click += new System.EventHandler(Manejadora.Manejador);
+            this.btnBoton.Click += new System.EventHandler(Manejadora.Manejador);
+            this.lblEtiqueta.Click += new System.EventHandler(man.ManejadorInstancia);
+        }
+
+        private void MostrarMensaje(object sender, EventArgs e)
+        {            
+            MessageBox.Show("Evento click del boton");
+            //this.lblEtiqueta.Click += new System.EventHandler(this.lblEtiqueta_Click);
+            //this.txtCuadroTexto.Click += new System.EventHandler(this.txtCuadroTexto_Click);
+        }
+
+        private void lblEtiqueta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click desde el label");
+            //this.lblEtiqueta.Click -= new System.EventHandler(this.lblEtiqueta_Click);
+        }
+
+        private void txtCuadroTexto_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evento del textBox");
+        }
+    }
+
+    public class Manejadora
+    {
+        public static void Manejador(object sender, EventArgs e)
+        {
+            MessageBox.Show("Estoy en el manejador estatico de la clase manejadora");
+        }
+
+        public void ManejadorInstancia(object sender, EventArgs e)
+        {
+            MessageBox.Show("Estoy en el metodo de instancia de la clase manejador");
+
+            if (sender is Label)
+                MessageBox.Show("El sender es " + sender.ToString());
+            else if (sender is TextBox)
+                MessageBox.Show("El sender es de tipo textbox");
+            else
+                MessageBox.Show("El sender es " + sender.ToString());
+        }
+
+        public static void Sumar(int a, int b)
+        {
+            MessageBox.Show((a + b).ToString());
+        }
+
+        public void Restar(int a, int b)
+        {
+            MessageBox.Show((a - b).ToString());
+        }
+
+        public void Multiplicar(int a, int b)
+        {
+            MessageBox.Show((a*b).ToString());
+        }
+    }
+}
---
+++ Clase24/FrmTest/FrmTest2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmTest
+{
+    public partial class FrmTest2 : Form
+    {
+        public FrmTest2()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Inicializar);
+        }
+
+        private void Inicializar(object sender, EventArgs e)
+        {
+            /*Manejadora man = new Manejadora();
+            MiDelegado delegado1 = new MiDelegado(Manejadora.Sumar);
+            MiDelegado delegado2 = new MiDelegado(man.Restar);
+
+            delegado1.Invoke(3, 5);//(explicita)
+            delegado1(2, 5);//(implicita) Las dos hacen lo mismo.*/
+            this.btnBoton1.Click += new System.EventHandler(this.MiManejador);
+            //MessageBox.Show();
+        }
+
+        public void MiManejador(object sender, EventArgs e)
+        {
+            string aux = ((Control)sender).Name;
+
+            MessageBox.Show(aux);
+            if (aux == this.btnBoton1.Name)
+            {
+                this.btnBoton1.Click -= new System.EventHandler(this.MiManejador);
+                this.btnBoton2.Click += new System.EventHandler(this.MiManejador);
+            }
+            else if (aux == this.btnBoton2.Name)
+            {
+                this.btnBoton2.Click -= new System.EventHandler(this.MiManejador);
+                this.btnBoton3.Click += new System.EventHandler(this.MiManejador);
+            }
+            else
+            {
+                this.btnBoton3.Click -= new System.EventHandler(this.MiManejador);
+                this.btnBoton1.Click += new System.EventHandler(this.MiManejador);
+            }
+        }
+
+        private void btnBoton4_Click(object sender, EventArgs e)
+        {
+            Manejadora man = new Manejadora();
+            MiDelegado delegado1 = new MiDelegado(Manejadora.Sumar);
+            MiDelegado delegado2 = new MiDelegado(man.Restar);
+            MiDelegado delegadoCombinado = new MiDelegado((MiDelegado)MiDelegado.Combine(delegado1, delegado2));
+            MiDelegado delegado4 = new MiDelegado((MiDelegado)MiDelegado.Combine(delegadoCombinado, new MiDelegado(man.Multiplicar)));
+
+            delegado1.Invoke(3, 5);//(explicita)
+            delegado2(2, 5);//(implicita) Las dos llaman a Invoke.
+            delegadoCombinado.Invoke(35,623);
+            MessageBox.Show(delegado1.Method.ToString());
+            MessageBox.Show(delegado2.Method.ToString());
+            MessageBox.Show(delegadoCombinado.Method.ToString());
+            //MessageBox.Show(delegado1.Target.ToString()); TIRA NULL EXCEPTION PORQUE ES ESTATICO
+            MessageBox.Show(delegado2.Target.ToString());
+            MessageBox.Show(delegadoCombinado.Target.ToString());
+            
+            foreach(Delegate item in delegado1.GetInvocationList())
+            {
+                MessageBox.Show(item.Method.ToString());
+            }
+
+            delegado4.Invoke(23, 52);
+
+            OtraSuma(delegado1, 2, 5);
+        }
+
+        public void OtraSuma(MiDelegado a, int g, int b)
+        {
+            a.Invoke(g, b);
+        }
+    }
+}
